badb7aa50314c0e917bab565de0d601684c154ac
Lucas Silva <lucas.silva@youxgroup.com.br>
2025-01-07 16:59:20 -0300

    initial commit

---
+++ CursoRest/src/main/java/com/lucassousa/CursoRest/OlaMundo.java
@@ -0,0 +1,13 @@
+package com.lucassousa.CursoRest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class OlaMundo {
+    public static void main( String[] args ) {
+    	Response request = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+    	System.out.println(request.getBody().asString());
+    
+    }
+}
